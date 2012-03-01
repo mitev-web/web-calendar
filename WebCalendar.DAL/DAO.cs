c3d6743ba136e02799e5e180ae277567a506ee99
Georgi Mitev <mitev.web@gmail.com>
2012-03-01 09:32:34 +0200

    ref

---
+++ WebCalendar.DAL/DAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace WebCalendar.DAL
+{
+    public class DAO
+    {
+        public static WebCalendarEntities db;
+
+        static DAO()
+        {
+            db = new WebCalendarEntities();
+        }
+    }
+}
---
+++ WebCalendar.UserTestData/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using WebCalendar.DAL;
+
+namespace WebCalendar.UserTestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
